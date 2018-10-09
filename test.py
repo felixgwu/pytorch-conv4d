f1b7824ac318609bdcfe61648b6eeef0f7e6c2b9
Felix Wu <felixgwu@gmail.com>
2018-10-08 21:25:56 -0400

    update test

--- test.py
+++ test.py
@@ -3,8 +3,8 @@ import time
 import torch.nn.functional as F
 
 k = 3
-B, C_in, W, H, U, V = 32, 4, 32, 32, 32, 32
-C_out = 4
+B, C_in, W, H, U, V = 32, 4, 5, 6, 7, 8
+C_out = 2
 
 weight = torch.rand(C_out, C_in, k, k, k, k)
 x = torch.rand(B, C_in, W, H, U, V)
@@ -19,7 +19,26 @@ print('unfold time:', end - start)
 
 start = time.perf_counter()
 
-o = torch.bmm(weight(1, C_out, C_in * k**4).repeat(B, 1, 1), x_unfold.view(B, C_in * k**4, W*H*U*V)).view(B, C_out, W, H, U, V)
+print(weight.size(), x_unfold.size())
+o = torch.einsum('oicdef,bicdefwhuv->bowhuv', (weight, x_unfold))
+print(o.size())
 
 end = time.perf_counter()
 print('conv time:', end - start)
+
+# start = time.perf_counter()
+# x_pad = F.pad(x, [k//2]*8)
+# W2, H2, U2, V2 = W + k - 1, H + k - 1, U + k - 1, V + k - 1
+# x_unfold = x_pad.as_strided([B, C_in, W, H, U, V, k, k, k, k], [C_in*W2*H2*U2*V2, W2*H2*U2*V2, H2*U2*V2, U2*V2, V2, 1, H2*U2*V2, U2*V2, V2, 1])
+
+# end = time.perf_counter()
+# print('unfold time:', end - start)
+
+# start = time.perf_counter()
+
+# print(weight.size(), x_unfold.size())
+# o = torch.einsum('oicdef,biwhuvcdef->bowhuv', (weight, x_unfold))
+# print(o.size())
+
+# end = time.perf_counter()
+# print('conv time:', end - start)
